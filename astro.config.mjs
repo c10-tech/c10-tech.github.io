a5aef0b81a45a9a6c891d522d95c16ef27eaba65
c10 <1352951127@qq.com>
2026-05-31 22:04:18 +0800

    更换风格

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://c10-tech.github.io',
+});
